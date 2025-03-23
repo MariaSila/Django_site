e94cce363d222c1d309b6de17a94ca775dfb34f7
MariaSila <dmn02@mail.ru>
2025-03-23 15:52:15 +0300

    Create shopapp

---
+++ mysite/shopapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+def shop_index(request: HttpRequest):
+    print(request.path)
+    print(request.method)
+    print(request.headers)
+    return HttpResponse('<h1>Hello World!</h1>')
+
+# Create your views here.
