d2761b713ad912fbf8812bc2e8ad85e03a299030
MariaSila <dmn02@mail.ru>
2025-03-28 03:48:04 +0300

    Learning django model, fields in ORM

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,5 +1,5 @@
 from timeit import default_timer
-
+from django.contrib.auth.models import Group
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
@@ -15,3 +15,10 @@ def shop_index(request: HttpRequest):
         'products': products,
     }
     return render(request, 'shopapp/shop-index.html', context=context)
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        'groups': Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)
