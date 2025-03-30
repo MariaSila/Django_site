be5d4b343ed81a7b67bc169d89197096729a41a2
MariaSila <dmn02@mail.ru>
2025-03-30 12:51:06 +0300

    Learning properties of fields, add fields: price, discount, created_at, archived

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import Group
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+from shopapp.models import Product
+
 
 def shop_index(request: HttpRequest):
     products = [
@@ -22,3 +24,10 @@ def groups_list(request: HttpRequest):
         'groups': Group.objects.prefetch_related('permissions').all(),
     }
     return render(request, 'shopapp/groups-list.html', context=context)
+
+
+def products_list(request: HttpRequest):
+    context = {
+        'products': Product.objects.all(),
+    }
+    return render(request, 'shopapp/products-list.html', context=context)
