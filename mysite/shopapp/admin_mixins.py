67785507a84fe0745f0cdc25cc90a6e3a1f7eef2
MariaSila <dmn02@mail.ru>
2025-03-31 16:11:28 +0300

    Connecting the model to the admin panel, filters, related entries, grouping, actions

---
+++ mysite/shopapp/admin_mixins.py
@@ -0,0 +1,24 @@
+import csv
+
+from django.db.models import QuerySet
+from django.http import HttpResponse, HttpRequest
+from django.db.models.options import Options
+
+
+class ExportAsCSVMixin:
+    def export_csv(self, request: HttpRequest, queryset: QuerySet):
+        meta: Options = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename={meta}-export.csv'
+
+        csv_write = csv.writer(response)
+        csv_write.writerow(field_names)
+
+        for obj in queryset:
+            csv_write.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_csv.short_description = 'Export as CSV'
