737c6639adff83eb1465f398f23fdecddf485d54
MariaSila <dmn02@mail.ru>
2025-04-05 08:51:10 +0300

    Lesson 8.2. Introduction to class View

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import Group
+
 from .models import Product
 
 
@@ -6,3 +8,9 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = 'name', 'price', 'description', 'discount'
+
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name']
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,29 +1,39 @@
 from timeit import default_timer
 from django.contrib.auth.models import Group
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.http import HttpResponse, HttpRequest
-
+from django.views import View
 from shopapp.models import Product, Order
-from .forms import ProductForm
-
-def shop_index(request: HttpRequest):
-    products = [
-        ('Laptop', 1999),
-        ('Desktop', 2999),
-        ('Smartphon', 999),
-    ]
-    context = {
-        'time_running': default_timer(),
-        'products': products,
-    }
-    return render(request, 'shopapp/shop-index.html', context=context)
+from .forms import ProductForm, GroupForm
 
 
-def groups_list(request: HttpRequest):
-    context = {
-        'groups': Group.objects.prefetch_related('permissions').all(),
-    }
-    return render(request, 'shopapp/groups-list.html', context=context)
+class ShopIndexView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        products = [
+            ('Laptop', 1999),
+            ('Desktop', 2999),
+            ('Smartphon', 999),
+        ]
+        context = {
+            'time_running': default_timer(),
+            'products': products,
+        }
+        return render(request, 'shopapp/shop-index.html', context=context)
+
+
+class GroupsListView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {
+            'form': GroupForm(),
+            'groups': Group.objects.prefetch_related('permissions').all(),
+        }
+        return render(request, 'shopapp/groups-list.html', context=context)
+
+    def post(self, request: HttpRequest):
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect(request.path)
 
 
 def products_list(request: HttpRequest):
@@ -31,6 +41,16 @@ def products_list(request: HttpRequest):
         'products': Product.objects.all(),
     }
     return render(request, 'shopapp/products-list.html', context=context)
+
+
+class ProductDetailsView(View):
+    def get(self,request: HttpRequest, pk: int) -> HttpResponse:
+        # product = Product.objects.get(pk=pk)
+        product = get_object_or_404(Product, pk=pk)
+        context = {
+            'product': product,
+        }
+        return render(request, 'shopapp/products-details.html', context=context)
 
 
 def create_product(request: HttpRequest) -> HttpResponse:
