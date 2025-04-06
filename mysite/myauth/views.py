1242817497dcdc97647e667968cc021a66b56f16
MariaSila <dmn02@mail.ru>
2025-04-06 14:30:09 +0300

    Lesson 9.3. class LoginView in myauth/urls.py

--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,21 +1,21 @@
-from django.contrib.auth import login, authenticate
-from django.http import HttpRequest
-from django.shortcuts import render, redirect
-
-
-def login_view(request: HttpRequest):
-    if request.method == 'GET':
-        if request.user.is_authenticated:
-            return redirect('/admin/')
-
-        return render(request, 'myauth/login.html')
-
-    username = request.POST['username']
-    password = request.POST['password']
-
-    user = authenticate(request, username=username, password=password)
-
-    if user is not None:
-        login(request, user)
-        return redirect('/admin/')
-    return render(request, 'myauth/login.html', {'error': 'Invalid login credentials'})
+# from django.contrib.auth import login, authenticate
+# from django.http import HttpRequest
+# from django.shortcuts import render, redirect
+#
+#
+# def login_view(request: HttpRequest):
+#     if request.method == 'GET':
+#         if request.user.is_authenticated:
+#             return redirect('/admin/')
+#
+#         return render(request, 'myauth/login.html')
+#
+#     username = request.POST['username']
+#     password = request.POST['password']
+#
+#     user = authenticate(request, username=username, password=password)
+#
+#     if user is not None:
+#         login(request, user)
+#         return redirect('/admin/')
+#     return render(request, 'myauth/login.html', {'error': 'Invalid login credentials'})
